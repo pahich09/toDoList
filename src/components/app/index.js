703c3d8c221dbea401333eae0caf3acff877bef0
Pavel <grebeniuk.p@gmail.com>
2020-02-18 08:53:56 +0200

    add maket

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -4,6 +4,18 @@ const App = () => {
   return (
     <div className="">
       <h1>Todo List App</h1>
+      <form>
+        <input type="text" />
+        <button>Add task</button>
+      </form>
+      <div>
+        <ul>
+          <li>Task #1</li>
+          <li>Task #2</li>
+          <li>Task #3</li>
+          <li>Task #4</li>
+        </ul>
+      </div>
     </div>
   );
 }
