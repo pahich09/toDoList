712f07095679bd643b0a17fc6f1c28ac1177dbee
Pavel <grebeniuk.p@gmail.com>
2020-02-18 09:54:33 +0200

    add new task

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,23 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const App = () => {
-  return (
-    <div className="">
-      <h1>Todo List App</h1>
-      <form>
-        <input type="text" />
-        <button>Add task</button>
-      </form>
-      <div>
-        <ul>
-          <li>Task #1</li>
-          <li>Task #2</li>
-          <li>Task #3</li>
-          <li>Task #4</li>
-        </ul>
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      tasks: [
+        { id: Math.random(), title: 'Task #1' },
+        { id: Math.random(), title: 'Task #2' },
+        { id: Math.random(), title: 'Task #3' },
+        { id: Math.random(), title: 'Task #4' },
+      ],
+    };
+    this.addNewTaskHendler = e => {
+      e.preventDefault();
+      console.log(this.state.tasks)
+      this.setState({
+        tasks: [
+          ...this.state.tasks,
+          { id: Math.random(), title: e.target[0].value }
+        ]
+      })
+      e.target[0].value = '';
+    };
+  }
+
+
+
+
+  render() {
+    const { tasks } = this.state;
+
+    return (
+      <div className="">
+        <h1>Todo List App</h1>
+        <form
+          onSubmit={this.addNewTaskHendler}
+        >
+          <input type="text" name="task" placeholder="Enter new task" />
+          <button>Add task</button>
+        </form>
+        <div>
+          <ul>
+            {
+              tasks.map(el => <li key={el.id}>{el.title}</li>)
+            }
+          </ul>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
